61ae12f4812c9f70caed0d6c4571494961183a66
unknown <sadmanhoque@yahoo.com>
2023-07-26 01:11:21 -0300

    last commit for the night, learning the fundementals of react components and tsx before starting to customize everything to be a portfolio

--- react-app/src/components/ListGroup.tsx
+++ react-app/src/components/ListGroup.tsx
@@ -1,12 +1,20 @@
+import { MouseEvent } from "react";
+
 function ListGroup() {
-  const items = ["New York", "San Fran", "Tokyo", "London"];
+  let items = ["New York", "San Fran", "Tokyo", "London"];
+
+  //Event handler
+  const handleClick = (event: MouseEvent) => console.log(event);
 
   return (
     <>
       <h1>List</h1>
+      {items.length === 0 && <p>No item found</p>}
       <ul className="list-group">
-        {items.map((item) => (
-          <li>{item}</li>
+        {items.map((item, index) => (
+          <li className="list-group-item" key={item} onClick={handleClick}>
+            {item}
+          </li>
         ))}
       </ul>
     </>
