201a08febe3f625d60488962e499c48f18274b8e
unknown <sadmanhoque@yahoo.com>
2023-07-29 21:13:19 -0300

    figured out how to change the theme for bootstrap

---
+++ mainCode/portfolio-project/src/App.tsx
@@ -0,0 +1,15 @@
+import "./App.css";
+
+function App() {
+  return (
+    <>
+      <div>
+        {/* Your app content goes here */}
+        <h1>Hello, React App!</h1>
+        {/* More components and content */}
+      </div>
+    </>
+  );
+}
+
+export default App;
