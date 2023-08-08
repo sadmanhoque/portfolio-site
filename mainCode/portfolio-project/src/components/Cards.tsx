fad04c9b849b0709acd42fe23459ff603fba09a5
unknown <sadmanhoque@yahoo.com>
2023-08-07 23:04:51 -0300

    the whole nested loop thing is too much work, just regular card component now that does nothing but make cards

--- mainCode/portfolio-project/src/components/Cards.tsx
+++ mainCode/portfolio-project/src/components/Cards.tsx
@@ -1,28 +1,22 @@
 //import { Link } from "react-router-dom";
 import "../App.css";
-import cardContents from "../text-files/workRecords.json";
 
 interface Props {
+  header: string;
   categories: string[];
 }
 
-function Cards({ categories }: Props) {
+function Cards({ categories, header }: Props) {
+  let boxContent = [];
+
   return (
     <div>
-      <ul>
-        {categories.map((item: string) => (
-          <li className="card-header">
-            <h1>{item}</h1>
-            <ul>
-              {cardContents.map((itemContent) => (
-                <li>
-                  <h4>{itemContent.DevOps}</h4>
-                </li>
-              ))}
-            </ul>
-          </li>
-        ))}
-      </ul>
+      <h1>{header}</h1>
+      {categories.map((category) => (
+        <li className="card-header">
+          <h1>{category}</h1>
+        </li>
+      ))}
     </div>
   );
 }
