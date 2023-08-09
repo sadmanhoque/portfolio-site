4cce591fcfdd69322ab8e7959b52ef35845e6477
unknown <sadmanhoque@yahoo.com>
2023-08-09 20:32:53 -0300

    was in the wrong dir when I git added before the last commit

---
+++ example-Copy/react-app/src/components/Cards.tsx
@@ -0,0 +1,22 @@
+//import { Link } from "react-router-dom";
+import "../App.css";
+
+interface Props {
+  header: string;
+  categories: string[];
+}
+
+function Cards({ categories, header }: Props) {
+  return (
+    <div className="card-header">
+      <h1>{header}</h1>
+      {categories.map((category) => (
+        <li key={category}>
+          <h4>{category}</h4>
+        </li>
+      ))}
+    </div>
+  );
+}
+
+export default Cards;
