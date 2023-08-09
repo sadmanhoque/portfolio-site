bbd73213fb23221beead8650ba8b8b0f55bad4d5
unknown <sadmanhoque@yahoo.com>
2023-08-08 22:57:40 -0300

    trying to figure out how to deal with the alignment issues

--- mainCode/portfolio-project/src/App.tsx
+++ mainCode/portfolio-project/src/App.tsx
@@ -1,6 +1,9 @@
 import "./App.css";
+
 import Cards from "./components/Cards";
 import Navbar from "./components/Navbar";
+import BootstrapCards from "./components/BootstrapCards";
+
 import wordRecords from "./text-files/workRecords.json";
 import projectRecords from "./text-files/projectRecords.json";
 
@@ -18,6 +21,12 @@ function App() {
     { section: "Projects", contents: ["one", "two", "three"] },
     { section: "Contacts", contents: ["aa", "bb", "cc"] },
   ];
+  const imageLink = [
+    "./src/assets/react.svg",
+    "./src/assets/react.svg",
+    "./src/assets/react.svg",
+    "./src/assets/react.svg",
+  ];
 
   return (
     <>
@@ -32,20 +41,36 @@ function App() {
           <Cards categories={wordRecords.IoT} header={"IoT"} />
           <Cards categories={wordRecords.BackEnd} header={"BackEnd"} />
         </div>
-        <div className="content">
-          <Cards
-            categories={projectRecords.GoogleMapsRandomizer}
-            header={"Google Maps Randomizer"}
-          />
-          <Cards
-            categories={projectRecords.DataAnalytics}
-            header={"Data Analytics"}
-          />
-          <Cards
-            categories={projectRecords.Electronics}
-            header={"Electronics"}
-          />
-          <Cards categories={projectRecords.Research} header={"Research"} />
+        <div className="content ">
+          <div className="container d-flex gx-6">
+            <BootstrapCards
+              header={"Google Maps Randomizer"}
+              stuff={projectRecords.GoogleMapsRandomizer[0]}
+              hyperLink={"google.com"}
+              imageLink={imageLink[0]}
+            />
+
+            <BootstrapCards
+              stuff={projectRecords.DataAnalytics[0]}
+              header={"Data Analytics"}
+              hyperLink={"google.com"}
+              imageLink={imageLink[0]}
+            />
+
+            <BootstrapCards
+              stuff={projectRecords.Electronics[0]}
+              header={"Electronics"}
+              hyperLink={"google.com"}
+              imageLink={imageLink[0]}
+            />
+
+            <BootstrapCards
+              stuff={projectRecords.Research[0]}
+              header={"Research"}
+              hyperLink={"google.com"}
+              imageLink={imageLink[0]}
+            />
+          </div>
         </div>
         <div className="content">
           <Cards categories={navBarItems} header={"temp"} />
