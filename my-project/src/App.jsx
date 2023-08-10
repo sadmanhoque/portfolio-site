4898d652c2db3744b38fa793b537c1934fd47aeb
unknown <sadmanhoque@yahoo.com>
2023-08-09 21:58:56 -0300

    added yet another site called my-project cause for whatever preexisting config the other one wasn't accepting tailwind classnames, this time using jsx

---
+++ my-project/src/App.jsx
@@ -0,0 +1,16 @@
+import React from 'react'
+import "./App.css"
+
+function App() {
+  return (
+    <div>
+      <section className="h-screen bg-green-400">
+        <p>
+          ;sldkfj;sdfhnpiwufhnpw;iorfnweriufn
+        </p>
+      </section>
+    </div>
+  )
+}
+
+export default App
